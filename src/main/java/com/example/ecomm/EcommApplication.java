5894e49260d6fe3a83dfd4153d2a5c39951efff3
Sk Alamin <alamin695sk@gmail.com>
2024-03-19 01:00:26 +0530

    inventory management controller functionality test

--- src/main/java/com/example/ecomm/EcommApplication.java
+++ src/main/java/com/example/ecomm/EcommApplication.java
@@ -1,13 +1,30 @@
 package com.example.ecomm;
 
+import com.example.ecomm.controllers.InventoryController;
+import com.example.ecomm.dtos.CreateOrUpdateRequestDto;
+import com.example.ecomm.dtos.CreateOrUpdateResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommApplication {
+public class EcommApplication implements CommandLineRunner {
+	@Autowired
+	InventoryController inventoryController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		CreateOrUpdateRequestDto requestDto = new CreateOrUpdateRequestDto();
+		requestDto.setUserId(1);
+		requestDto.setProductId(1);
+		requestDto.setQuantity(1000);
+		CreateOrUpdateResponseDto responseDto = inventoryController.createOrUpdateInventory(requestDto);
+		System.out.println(responseDto);
+
+	}
 }
