088c4db31c5d2423073ce90948b1da6493c01cdc
Sk Alamin <alamin695sk@gmail.com>
2024-03-20 22:24:44 +0530

    inventory management place order functionality testing done

--- src/main/java/com/example/ecomm/EcommApplication.java
+++ src/main/java/com/example/ecomm/EcommApplication.java
@@ -1,17 +1,27 @@
 package com.example.ecomm;
 
 import com.example.ecomm.controllers.InventoryController;
+import com.example.ecomm.controllers.OrderController;
 import com.example.ecomm.dtos.CreateOrUpdateRequestDto;
 import com.example.ecomm.dtos.CreateOrUpdateResponseDto;
+import com.example.ecomm.dtos.PlaceOrderRequestDto;
+import com.example.ecomm.dtos.PlaceOrderResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class EcommApplication implements CommandLineRunner {
 	@Autowired
 	InventoryController inventoryController;
+
+	@Autowired
+	OrderController orderController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommApplication.class, args);
@@ -19,12 +29,24 @@ public class EcommApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		CreateOrUpdateRequestDto requestDto = new CreateOrUpdateRequestDto();
-		requestDto.setUserId(1);
-		requestDto.setProductId(1);
-		requestDto.setQuantity(1000);
-		CreateOrUpdateResponseDto responseDto = inventoryController.createOrUpdateInventory(requestDto);
+//		CreateOrUpdateRequestDto requestDto = new CreateOrUpdateRequestDto();
+//		requestDto.setUserId(1);
+//		requestDto.setProductId(1);
+//		requestDto.setQuantity(1000);
+//		CreateOrUpdateResponseDto responseDto = inventoryController.createOrUpdateInventory(requestDto);
+//		System.out.println(responseDto);
+		PlaceOrderRequestDto placeOrderRequestDto = new PlaceOrderRequestDto();
+		placeOrderRequestDto.setUserId(1);
+		placeOrderRequestDto.setAddressId(1);
+		List<Pair<Integer, Integer>> lists = new ArrayList<>();
+		lists.add(Pair.of(1, 10));
+		lists.add(Pair.of(2, 117));
+		lists.add(Pair.of(3, 124));
+		lists.add(Pair.of(4, 105));
+		placeOrderRequestDto.setOrderDetails(lists);
+		PlaceOrderResponseDto responseDto = orderController.placeOrder(placeOrderRequestDto);
 		System.out.println(responseDto);
+
 
 	}
 }
